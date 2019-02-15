3f919af3809a78a62113527e18f851f77711246d
Di14Mehdi <45431651+Di14Mehdi@users.noreply.github.com>
2019-02-15 09:18:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -28,9 +28,84 @@ while ($row = mysql_fetch_assoc($result)) {
     echo $row["total"]. "<br>";
     $rowcnt++;
 }
-/* COMMENT: Clean-up and close */
+/*PIE CHART TRY*/
+<?php
+// Connecting, selecting database
+$link = mysql_connect('mysql_host', 'mysql_user', 'mysql_password')
+    or die('Could not connect: ' . mysql_error());
+mysql_select_db('my_database') or die('Could not select database');
+// Performing SQL query
+$query =
+'SELECT ip_address, ip_count FROM `dump_info` order by ip_count desc group by ip_address limit 5';
+$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+// Printing results inside an HTML infographic tag
+echo "<infographic-piechart  width='300'  height='300'>\n";
+echo "<infographic-data>\n";
+while ($row = mysql_fetch_array($result)) {
+    echo "<infographic-pieslice value='" .
+         $row['ip_count'] . "'>" .
+         $row['ip_address'] .
+         "</infographic-pieslice>\n";
+}
+echo "<infographic-data>\n";
+echo "</infographic-piechart>\n";
+// Free resultset
 mysql_free_result($result);
-mysql_close($dbconn);
-echo "MYSQL_CLOSE Success: $rowcnt <br>";
+// Closing connection
+mysql_close($link);
+?>
+
+
+
+/*Pie CHART*/
+/*
+//include the library
+    include "libchart/libchart/classes/libchart.php";
+ 
+    //new pie chart instance
+    $chart = new PieChart( 500, 300 );
+ 
+    //data set instance
+    $dataSet = new XYDataSet();
+    
+    //actual data
+    //get data from the database
+    
+    //include database connection
+    include 'db_connect.php';
+ 
+    //query all records from the database
+    $query = "select * from dumps";
+ 
+    //execute the query
+    $result = $mysqli->query( $query );
+ 
+    //get number of rows returned
+    $num_results = $result->num_rows;
+ 
+    if( $num_results > 0){
+    
+        while( $row = $result->fetch_assoc() ){
+            extract($row);
+            $dataSet->addPoint(new Point("{$error} {$total})", $total));
+        }
+    
+        //finalize dataset
+        $chart->setDataSet($dataSet);
+ 
+        //set chart title
+        $chart->setTitle("Pie Chart test");
+        
+        //render as an image and store under "generated" folder
+        $chart->render("testpie/1.png");
+    
+        //pull the generated chart where it was stored
+        echo "<img alt='Pie chart'  src='testpie/1.png' style='border: 1px solid gray;'/>";
+    
+    }else{
+        echo "Error";
+    }
+*/
+/*footer code*/
 include('footer.html');
 ?>
