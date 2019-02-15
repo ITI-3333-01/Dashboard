eab6a63b1ac787a0c1040c7055a4459c1dbc1dbc
Di14Mehdi <45431651+Di14Mehdi@users.noreply.github.com>
2019-02-15 13:33:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -28,34 +28,27 @@ if (!$dbselect) {
         var data = google.visualization.arrayToDataTable([
           ['Domain', 'Total Packets'],
         <?php 
-$exec = mysql_query("SELECT total,time FROM dumps WHERE time >DATE_SUB(CURDATE(), INTERVAL 1 hour) ORDER BY total DESC LIMIT 6;"); 
-            
+//WHERE time >DATE_SUB(CURDATE(), INTERVAL 1 hour)
+$exec = mysql_query("SELECT total,time FROM dumps  ORDER BY total DESC LIMIT 6;"); 
                if (!$exec) {
     die("Database query failed: " . mysql_error());
 }
- 
-/* $row = mysql_fetch_array($exec);
-             if (!$row) {
-    die("Database fetch failed: " . mysql_error());
-}*/
   while($row = mysql_fetch_array($exec)){
   echo "['".$row["time"]."', ".$row["total"]."],";
   }
  ?>
         ]);
-
         var options = {
           title: 'Total packets by time'
         };
-
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
         chart.draw(data, options);
       }
     </script>
   </head>
   <body>
     <div id="piechart" style="width: 900px; height: 500px;"></div>
+      <div id="piechart" style="width: 900px; height: 500px;"></div>
   </body>
 <?php
     mysql_free_result($result);
