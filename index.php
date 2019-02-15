c9a424eb83cdb527953a23e4fa48ad05394dac51
Di14Mehdi <45431651+Di14Mehdi@users.noreply.github.com>
2019-02-15 08:37:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,56 +4,33 @@ $servername= "localhost";
 $username="flare";
 $password="Flare-3333";
 $database="flare";
-
 /* COMMENT: Connect to the mysql server */
 $dbconn = mysql_connect($servername, $username, $password);
 if(!$dbconn) {
     die("Connection failed: " . mysql_error());
 }
 echo "MYSQL_CONNECT Success <br>";
-
 /* COMMENT: Select the database within the server */
 $dbselect = mysql_select_db($database, $dbconn);
 if (!$dbselect) {
     die("Database select failed: " . mysql_error());
 }
 echo "MYSQL_SELECT_DB Success <br>";
-
 /* COMMENT: Setup the query to SELECT * from dumps */
 $result = mysql_query("SELECT * FROM dumps WHERE time >DATE_SUB(CURDATE(), INTERVAL 1 hour) ORDER BY total DESC LIMIT 5;");
 if (!result) {
     die("Database select failed: " . mysql_error());
 }
 echo "MYSQL_QUERY Success <br>";
-
 /* COMMENT: Loop through all rows returned */
 $rowcnt = 0;
 while ($row = mysql_fetch_assoc($result)) {
     echo $row["total"]. "<br>";
     $rowcnt++;
 }
-
-
-// Performing SQL query
-$query =
-'SELECT total,time FROM dumps WHERE time >DATE_SUB(CURDATE(), INTERVAL 1 hour) ORDER BY total DESC LIMIT 5';
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-
-// Printing results inside an HTML infographic tag
-echo "<infographic-piechart  width='300'  height='300'>\n";
-echo "<infographic-data>\n";
-while ($row = mysql_fetch_array($result)) {
-    echo "<infographic-pieslice value='" .
-         $row['total'] . "'>" .
-         $row['time'] .
-         "</infographic-pieslice>\n";
-}
-echo "<infographic-data>\n";
-echo "</infographic-piechart>\n";
-
-// Free resultset
+/* COMMENT: Clean-up and close */
 mysql_free_result($result);
-
-/*footer code*/
+mysql_close($dbconn);
+echo "MYSQL_CLOSE Success: $rowcnt <br>";
 include('footer.html');
 ?>
