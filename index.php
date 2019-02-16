66771a761e7b9abd0dcc96f87059392d14dc99f0
Di14Mehdi <45431651+Di14Mehdi@users.noreply.github.com>
2019-02-16 15:07:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -22,55 +22,35 @@ if (!$dbselect) {
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart1() {
-
+      function drawChart() {
         var data = google.visualization.arrayToDataTable([
           ['Domain', 'Total Packets'],
         <?php 
-//WHERE time >DATE_SUB(CURDATE(), INTERVAL 1 hour)
-$exec = mysql_query("SELECT total,time FROM dumps  ORDER BY total DESC LIMIT 6;"); 
+$exec = mysql_query("SELECT total,time FROM dumps WHERE time >DATE_SUB(CURDATE(), INTERVAL 1 hour) ORDER BY total DESC LIMIT 6;"); 
+            
                if (!$exec) {
     die("Database query failed: " . mysql_error());
 }
+ 
+/* $row = mysql_fetch_array($exec);
+             if (!$row) {
+    die("Database fetch failed: " . mysql_error());
+}*/
   while($row = mysql_fetch_array($exec)){
   echo "['".$row["time"]."', ".$row["total"]."],";
   }
  ?>
         ]);
         var options = {
-          title: 'Total packets by time', width:400,
-                       height:300
+          title: 'Total packets by time'
         };
-        var chart = new google.visualization.PieChart(document.getElementById('piechart1'));
-        chart.draw(data, options);
-      }
-        function drawChart2() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Domain', 'Total Packets'],
-        <?php 
-//WHERE time >DATE_SUB(CURDATE(), INTERVAL 1 hour)
-$exec = mysql_query("SELECT total,time FROM dumps  ORDER BY total DESC LIMIT 6;"); 
-               if (!$exec) {
-    die("Database query failed: " . mysql_error());
-}
-  while($row = mysql_fetch_array($exec)){
-  echo "['".$row["time"]."', ".$row["total"]."],";
-  }
- ?>
-        ]);
-        var options = {
-          title: 'Total packets by time', width:400, 
-                       height:300
-        };
-        var chart = new google.visualization.PieChart(document.getElementById('piechart2'));
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
         chart.draw(data, options);
       }
     </script>
   </head>
- <body>
-    <div id="piechar1t" style="width: 900px; height: 500px;"></div>
+  <body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
   </body>
 <?php
     mysql_free_result($result);
