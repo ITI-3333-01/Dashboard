e77759e1916c8de2e31fc26ef981f5e2d65b0110
Di14Mehdi <45431651+Di14Mehdi@users.noreply.github.com>
2019-04-26 13:18:55 -0500

    Update graph.php

--- graph.php
+++ graph.php
@@ -1,7 +1,28 @@
+<?php 
+ include('header.html');
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Number Field</h2>
+<p>The <strong>input type="number"</strong> defines a numeric input field.</p>
+<p>You can use the min and max attributes to add numeric restrictions in the input field:</p>
+
+<form action="/graph.php" method="get">
+  Quantity (between 1 and 5):
+  <input type="number" name="quantity" min="1" max="5">
+  <input type="submit">
+</form>
+
+<p><b>Note:</b> type="number" is not supported in IE9 and earlier.</p>
+
+</body>
+</html>
 <?php
   $page = $_SERVER['PHP_SELF'];
   $sec = "60";
-  include('header.html');
+ 
   $servername= "localhost";
   $username="flare";
   $password="Flare-3333";
@@ -17,24 +38,7 @@
     die("Database select failed: " . mysql_error());
   }
 ?>
-<!DOCTYPE html>
-<html>
-<body>
 
-<h2>Number Field</h2>
-<p>The <strong>input type="number"</strong> defines a numeric input field.</p>
-<p>You can use the min and max attributes to add numeric restrictions in the input field:</p>
-
-<form action="/graph.php">
-  Quantity (between 1 and 5):
-  <input type="number" name="quantity" min="1" max="5">
-  <input type="submit">
-</form>
-
-<p><b>Note:</b> type="number" is not supported in IE9 and earlier.</p>
-
-</body>
-</html>
 
 <html>
 <head>
